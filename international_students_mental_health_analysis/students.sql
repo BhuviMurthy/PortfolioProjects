3df766e427ca62eb46d69e2f343a9ca843fe6067
Bhuvaneshwari Murthy <121146581+BhuviMurthy@users.noreply.github.com>
2024-07-30 01:02:17 -0500

    Update students.sql

--- international_students_mental_health_analysis/students.sql
+++ international_students_mental_health_analysis/students.sql
@@ -1,1 +1,19 @@
+-- Explore and analyze the students data to see how the length of stay (stay) impacts the average mental health diagnostic scores of the international students present in the study.
 
+-- Return a table with nine rows and five columns.
+-- The five columns should be aliased as: stay, count_int, average_phq, average_scs, and average_as, in that order.
+-- The average columns should contain the average of the todep (PHQ-9 test), tosc (SCS test), and toas (ASISS test) columns for each length of stay, rounded to two decimal places.
+-- The count_int column should be the number of international students for each length of stay.
+-- Sort the results by the length of stay in descending order.
+
+SELECT *
+FROM students;
+
+select stay, count(*) as count_int, 
+round(avg(todep),2) as average_phq, 
+round(avg(tosc),2) as average_scs, 
+round(avg(toas),2) as average_as
+from students 
+WHERE inter_dom = 'Inter'
+group by stay
+order by stay desc
